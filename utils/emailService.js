247514caeaf72374826b40356f2bd00e2e9499e9
pouiks <virgilejoinville@gmail.com>
2025-02-10 12:02:49 +0100

    modifying email send

--- utils/emailService.js
+++ utils/emailService.js
@@ -7,25 +7,17 @@ exports.sendEmail = async (email, link) => {
   console.log("Tentative d'envoi de mail...");
   try {
     const request = mailjet.post("send", { version: "v3.1" }).request({
-      Messages: [
-        {
-          From: {
-            Email: process.env.EMAIL_USER,
-            Name: "Uxco group",
-          },
-          To: [
-            {
-              Email: email,
-            },
-          ],
-          TemplateID: 6602291,
-          TemplateLanguage: true,
-          Subject: "Finalisez votre dossier de réservation pour votre appartement",
-          Variables: {
-            link, // Assurez-vous que "link" est correctement passé ici
-          },
-        },
-      ],
+      headers: {
+        "Authorization": "Basic " + Buffer.from(`${process.env.MAILJET_API_KEY}:${process.env.MAILJET_API_SECRET}`).toString("base64")
+      },
+      Messages: [{
+        From: { Email: process.env.EMAIL_USER, Name: "Uxco group" },
+        To: [{ Email: email }],
+        TemplateID: 6602291,
+        TemplateLanguage: true,
+        Subject: "Finalisez votre dossier",
+        Variables: { link },
+      }]
     });
 
     const response = await request;
